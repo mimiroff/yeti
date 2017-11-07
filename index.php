0e1bfd628093e4efc10d167c14ca5d1a00d0c0da
Ilia Barashkov <ibarashkov@list.ru>
2017-11-07 23:43:48 +0300

    Task 1.2

--- index.php
+++ index.php
@@ -42,9 +42,26 @@ $lot_time_remaining = gmdate("H:i:s", $tomorrow - $now);
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
-
+        <?php
+          if ($is_auth) {
+          echo '<div class="user-menu__image">
+              <img src="'.$user_avatar.'" width="40" height="40" alt="Пользователь">
+            </div>
+            <div class="user-menu__logged">
+              <p>'.$user_name.'</p>
+            </div>';
+          }
+          else {
+           echo '<ul class="user-menu__list">
+            <li class="user-menu__item">
+              <a href="#">Регистрация</a>
+            </li>
+            <li class="user-menu__item">
+              <a href="#">Вход</a>
+            </li>
+          </ul>';
+          }     
+?>
         </nav>
     </div>
 </header>
