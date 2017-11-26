378f2317c07599b4dccf488e3f75248747504608
Ilia Barashkov <ibarashkov@list.ru>
2017-11-26 20:11:21 +0300

    More fixes

--- schema.sql
+++ schema.sql
@@ -1,41 +1,41 @@
 CREATE TABLE categories (
-  id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  category CHAR(128)
+  id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  category VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE users (
   id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255),
-  password CHAR(32),
-  register_data DATETIME,
-  name CHAR(64),
-  picture CHAR(255),
-  contacts TEXT,
+  email VARCHAR(255) NOT NULL,
+  password CHAR(60) NOT NULL,
+  register_data DATETIME NOT NULL,
+  name VARCHAR(128) NOT NULL,
+  picture VARCHAR(255),
+  contacts TEXT NOT NULL,
   is_deleted TINYINT(1) UNSIGNED
 );
 
 CREATE TABLE bets (
-  id INT(50) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  date DATETIME,
-  cost INT UNSIGNED,
-  user_id INT UNSIGNED,
-  lot_id INT UNSIGNED,
+  id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  date DATETIME NOT NULL,
+  cost INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  lot_id INT UNSIGNED NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (lot_id) REFERENCES lots(id)
 );
 
 CREATE TABLE lots (
-  id INT(50) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  date DATETIME,
-  name CHAR(255),
-  description TEXT,
-  picture CHAR(255),
-  price INT UNSIGNED,
-  expire_date DATETIME,
-  step INT UNSIGNED,
+  id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  date DATETIME NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  picture VARCHAR(255) NOT NULL,
+  price INT UNSIGNED NOT NULL,
+  expire_date DATETIME NOT NULL,
+  step INT UNSIGNED DEFAULT 500,
   author_id INT UNSIGNED,
   winner_id INT UNSIGNED,
-  category_id INT UNSIGNED,
+  category_id INT UNSIGNED NOT NULL,
   FOREIGN KEY (author_id, winner_id) REFERENCES users(id),
   FOREIGN KEY (category_id) REFERENCES categories(id)
 );
@@ -47,3 +47,5 @@ CREATE INDEX b_date ON bets(date);
 CREATE INDEX l_date ON lots(date);
 CREATE INDEX l_exp ON lots(expire_date);
 CREATE INDEX l_price ON lots(price);
+CREATE INDEX l_name ON lots(name);
+CREATE INDEX l_desc ON lots(description);
